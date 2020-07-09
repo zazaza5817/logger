b7c664c2daf63612dcc7ef2363e3ae431173ad06
zazaza5817 <49568244+zazaza5817@users.noreply.github.com>
2020-07-09 22:27:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 import logging
+
+logging.basicConfig(filename='latest.log', filemode='a', format='[%(asctime)s][%(levelname)s]: %(message)s', level=logging.INFO)
+logging.basicConfig(filename='latest.log', filemode='a', format='[%(asctime)s]: %(message)s', level=logging.WARNING)
+logging.warning('This will get logged to a file')
+logging.info('This will get logged to a file')
